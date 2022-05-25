337f8581b03fa64547a14c02ea21d1772dd19818
Nurjigit <alymbekovnurjigit01@gmail.com>
2022-05-26 02:04:29 +0600

    Добавьте файлы проекта.

---
+++ WinFormsProjectWithThreads/Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿
+namespace WinFormsProjectWithThreads
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.BubbleSorting = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.InsertionSorting = new System.Windows.Forms.ListBox();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SelectionSorting = new System.Windows.Forms.ListBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker3 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker4 = new System.ComponentModel.BackgroundWorker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Array size:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(84, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(204, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Generate an array";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(440, 21);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Sort";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(540, 21);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Stop";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // BubbleSorting
+            // 
+            this.BubbleSorting.FormattingEnabled = true;
+            this.BubbleSorting.ItemHeight = 15;
+            this.BubbleSorting.Location = new System.Drawing.Point(355, 86);
+            this.BubbleSorting.Name = "BubbleSorting";
+            this.BubbleSorting.Size = new System.Drawing.Size(100, 184);
+            this.BubbleSorting.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(355, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Bubble sorting";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(355, 276);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.TabIndex = 7;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(484, 276);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(100, 23);
+            this.progressBar2.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(484, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 15);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Insertion sorting";
+            // 
+            // InsertionSorting
+            // 
+            this.InsertionSorting.FormattingEnabled = true;
+            this.InsertionSorting.ItemHeight = 15;
+            this.InsertionSorting.Location = new System.Drawing.Point(484, 86);
+            this.InsertionSorting.Name = "InsertionSorting";
+            this.InsertionSorting.Size = new System.Drawing.Size(100, 184);
+            this.InsertionSorting.TabIndex = 8;
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.Location = new System.Drawing.Point(612, 276);
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.Size = new System.Drawing.Size(100, 23);
+            this.progressBar3.TabIndex = 13;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(612, 65);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 15);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Selection sorting";
+            // 
+            // SelectionSorting
+            // 
+            this.SelectionSorting.FormattingEnabled = true;
+            this.SelectionSorting.ItemHeight = 15;
+            this.SelectionSorting.Location = new System.Drawing.Point(612, 86);
+            this.SelectionSorting.Name = "SelectionSorting";
+            this.SelectionSorting.Size = new System.Drawing.Size(100, 184);
+            this.SelectionSorting.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(612, 302);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(95, 15);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Selection sorting";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(484, 302);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(93, 15);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Insertion sorting";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(355, 302);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(84, 15);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Bubble sorting";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.WorkerReportsProgress = true;
+            this.backgroundWorker2.WorkerSupportsCancellation = true;
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
+            this.backgroundWorker2.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker2_ProgressChanged);
+            this.backgroundWorker2.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker2_RunWorkerCompleted);
+            // 
+            // backgroundWorker3
+            // 
+            this.backgroundWorker3.WorkerReportsProgress = true;
+            this.backgroundWorker3.WorkerSupportsCancellation = true;
+            this.backgroundWorker3.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker3_DoWork);
+            this.backgroundWorker3.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker3_ProgressChanged);
+            this.backgroundWorker3.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker3_RunWorkerCompleted);
+            // 
+            // backgroundWorker4
+            // 
+            this.backgroundWorker4.WorkerReportsProgress = true;
+            this.backgroundWorker4.WorkerSupportsCancellation = true;
+            this.backgroundWorker4.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker4_DoWork);
+            this.backgroundWorker4.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker4_ProgressChanged);
+            this.backgroundWorker4.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker4_RunWorkerCompleted);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 65);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 15);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Source data";
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.ItemHeight = 15;
+            this.listBox4.Location = new System.Drawing.Point(20, 86);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(100, 184);
+            this.listBox4.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 376);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.progressBar3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.SelectionSorting);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.InsertionSorting);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.BubbleSorting);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox BubbleSorting;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox InsertionSorting;
+        private System.Windows.Forms.ProgressBar progressBar3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox SelectionSorting;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker3;
+        private System.ComponentModel.BackgroundWorker backgroundWorker4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox listBox4;
+    }
+}
+
---
+++ WinFormsProjectWithThreads/Form1.cs
@@ -0,0 +1,375 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsProjectWithThreads
+{
+    public partial class Form1 : Form
+    {
+        int[] array;
+        int[] arrayBub;
+        int[] arrayIns;
+        int[] arraySel;
+
+        TimeSpan tsBubble;
+        TimeSpan tsIns;
+        TimeSpan tsSel;
+
+        bool fCancelBub;
+        bool fCancelIns;
+        bool fCancelSel;
+
+        private void DisplayArray(int[] A, ListBox LB)
+        {
+            LB.Items.Clear();
+            for (int i = 0; i < A.Length; i++)
+            {
+                LB.Items.Add(A[i]);
+            }
+        }
+
+        private void Active(bool active)
+        {
+            label2.Enabled = active;
+            label3.Enabled = active;
+            label4.Enabled = active;
+            label5.Enabled = active;
+            label6.Enabled = active;
+            label7.Enabled = active;
+            label8.Enabled = active;
+            BubbleSorting.Enabled = active;
+            InsertionSorting.Enabled = active;
+            SelectionSorting.Enabled = active;
+            progressBar1.Enabled = active;
+            progressBar2.Enabled = active;
+            progressBar3.Enabled = active;
+            button2.Enabled = active;
+            button3.Enabled = active;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Active(false);
+
+            label6.Text = "";
+            label7.Text = "";
+            label8.Text = "";
+
+            if (!backgroundWorker1.IsBusy)
+            {
+                backgroundWorker1.RunWorkerAsync();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+
+            if (!backgroundWorker2.IsBusy)
+            {
+                backgroundWorker2.RunWorkerAsync();
+            }
+
+            if (!backgroundWorker3.IsBusy)
+            {
+                backgroundWorker3.RunWorkerAsync();
+            }
+
+            if (!backgroundWorker4.IsBusy)
+            {
+                backgroundWorker4.RunWorkerAsync();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                backgroundWorker2.CancelAsync();
+                backgroundWorker3.CancelAsync();
+                backgroundWorker4.CancelAsync();
+            }
+            catch (InvalidOperationException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Random rnd = new Random();
+
+            int n = Convert.ToInt32(textBox1.Text);
+
+            array = new int[n];
+
+            arrayBub = new int[n];
+            arrayIns = new int[n];
+            arraySel = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Thread.Sleep(1);
+                array[i] = rnd.Next(1, n + 1);
+                arrayBub[i] = arraySel[i] = arrayIns[i] = array[i];
+
+                try
+                {
+                    backgroundWorker1.ReportProgress((i * 100) / n);
+                }
+                catch (InvalidOperationException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    return;
+                }
+            }
+        }
+
+        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+        {
+            int x;
+
+            tsBubble = new TimeSpan(DateTime.Now.Ticks);
+
+            for (int i = 0; i < arrayBub.Length; i++)
+            {
+                Thread.Sleep(1);
+
+                for (int j = arrayBub.Length - 1; j > i; j--)
+                {
+                    if (arrayBub[j - 1] > arrayBub[j])
+                    {
+                        x = arrayBub[j];
+                        arrayBub[j] = arrayBub[j - 1];
+                        arrayBub[j - 1] = x;
+                    }
+                }
+
+                try
+                {
+                    backgroundWorker2.ReportProgress((i * 100) / arrayBub.Length);
+                }
+                catch (InvalidOperationException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    return;
+                }
+
+                if (backgroundWorker2.CancellationPending)
+                {
+                    fCancelBub = true;
+                    break;
+                }
+            }
+        }
+
+        private void backgroundWorker3_DoWork(object sender, DoWorkEventArgs e)
+        {
+            int x, i, j;
+
+            tsIns = new TimeSpan(DateTime.Now.Ticks);
+
+            for (i = 0; i < arrayIns.Length; i++)
+            {
+                Thread.Sleep(1);
+
+                x = arrayIns[i];
+
+                for (j = i - 1; j >= 0 && arrayIns[j] > x; j--)
+                    arrayIns[j + 1] = arrayIns[j];
+                arrayIns[j + 1] = x;
+
+                try
+                {
+                    backgroundWorker3.ReportProgress((i * 100) / arrayIns.Length);
+                }
+                catch (InvalidOperationException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    return;
+                }
+
+                if (backgroundWorker3.CancellationPending)
+                {
+                    fCancelIns = true;
+                    break;
+                }
+            }
+        }
+
+        private void backgroundWorker4_DoWork(object sender, DoWorkEventArgs e)
+        {
+            int i, j, k;
+            int x;
+
+            tsSel = new TimeSpan(DateTime.Now.Ticks);
+
+            for (i = 0; i < arraySel.Length; i++)
+            {
+                Thread.Sleep(1);
+
+                k = i;
+
+                x = arraySel[i];
+
+                for (j = i + 1; j < arraySel.Length; j++)
+                    if (arraySel[j] < x)
+                    {
+                        k = j;
+                        x = arraySel[j];
+                    }
+
+                arraySel[k] = arraySel[i];
+                arraySel[i] = x;
+
+                try
+                {
+                    backgroundWorker4.ReportProgress((i * 100) / arraySel.Length);
+                }
+                catch (InvalidOperationException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    return;
+                }
+
+                if (backgroundWorker4.CancellationPending)
+                {
+                    fCancelSel = true;
+                    break;
+                }
+            }
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            button1.Text = "Generate array " + e.ProgressPercentage.ToString() + "%";
+        }
+
+        private void backgroundWorker2_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            label8.Text = Convert.ToString(e.ProgressPercentage) + " %";
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void backgroundWorker3_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            label7.Text = Convert.ToString(e.ProgressPercentage) + " %";
+            progressBar2.Value = e.ProgressPercentage;
+        }
+
+        private void backgroundWorker4_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            label6.Text = Convert.ToString(e.ProgressPercentage) + " %";
+            progressBar3.Value = e.ProgressPercentage;
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            button1.Text = "Generate array";
+
+            Active(true);
+
+            DisplayArray(array, BubbleSorting);
+            DisplayArray(array, InsertionSorting);
+            DisplayArray(array, SelectionSorting);
+        }
+
+        private void backgroundWorker2_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (fCancelBub)
+            {
+                label8.Text = "";
+
+                DisplayArray(array, BubbleSorting);
+
+                fCancelBub = false;
+            }
+            else
+            {
+                TimeSpan time = new TimeSpan(DateTime.Now.Ticks) - tsBubble;
+                label8.Text = String.Format("{0}:{1}:{2}:{3}", time.Hours, time.Minutes,
+                time.Seconds, time.Milliseconds);
+
+                DisplayArray(arrayBub, BubbleSorting);
+            }
+
+            progressBar1.Value = 0;
+            button1.Enabled = true;
+        }
+
+        private void backgroundWorker3_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (fCancelIns)
+            {
+                label7.Text = "";
+
+                DisplayArray(array, InsertionSorting);
+
+                fCancelIns = false;
+            }
+            else
+            {
+                TimeSpan time = new TimeSpan(DateTime.Now.Ticks) - tsIns;
+                label7.Text = String.Format("{0}:{1}:{2}:{3}", time.Hours, time.Minutes,
+                time.Seconds, time.Milliseconds);
+
+                DisplayArray(arrayIns, InsertionSorting);
+            }
+
+            progressBar2.Value = 0;
+            button1.Enabled = true;
+        }
+
+        private void backgroundWorker4_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (fCancelSel)
+            {
+                label6.Text = "";
+
+                DisplayArray(array, SelectionSorting);
+
+                fCancelSel = false;
+            }
+            else
+            {
+                TimeSpan time = new TimeSpan(DateTime.Now.Ticks) - tsSel;
+                label6.Text = String.Format("{0}:{1}:{2}:{3}", time.Hours, time.Minutes,
+                  time.Seconds, time.Milliseconds);
+
+                DisplayArray(arraySel, SelectionSorting);
+            }
+
+            progressBar3.Value = 0;
+            button1.Enabled = true;
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = "";
+
+            BubbleSorting.Items.Clear();
+            InsertionSorting.Items.Clear();
+            SelectionSorting.Items.Clear();
+
+            progressBar1.Value = 0;
+            progressBar2.Value = 0;
+            progressBar3.Value = 0;
+
+            Active(false);
+
+            fCancelBub = false;
+            fCancelIns = false;
+            fCancelSel = false;
+
+        }
+    }
+}
